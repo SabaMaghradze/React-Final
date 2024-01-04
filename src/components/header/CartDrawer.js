b49a5f7cbc22d6838200f6fd5d12ddf572a7126e
SabaMaghradze <sabamaghradze1001@gmail.com>
2024-01-05 02:33:47 +0400

    cartDrawer bug fix

--- src/components/header/CartDrawer.js
+++ src/components/header/CartDrawer.js
@@ -4,6 +4,7 @@ import { LoadingWrapper } from "../atoms";
 import { useDispatch } from "react-redux";
 import { clearCart, saveCart } from "../../redux/slices/cartSlice";
 import { Button } from "../atoms";
+import { useTranslation } from "react-i18next";
 
 const StyledCartItem = styled(Box)(() => ({
     width: 400,
@@ -30,39 +31,44 @@ export const CartDrawer = ({ isCartOpen, setIsCartOpen, cartItems }) => {
     const { userData } = useUser();
     const dispatch = useDispatch();
 
+    const { t } = useTranslation();
+
     const { loading } = useCart();
 
     return (
         <Drawer open={isCartOpen} onClose={() => setIsCartOpen(false)} anchor="right" >
             <LoadingWrapper isLoading={loading}>
-                {cartItems && Array.isArray(cartItems) ? (
+                {cartItems && Array.isArray(cartItems) && cartItems.length > 0 ? (
                     cartItems.map((item) => {
                         const { product, quantity } = item;
                         const { name, _id, price, image } = product;
                         return (
-                            <StyledCartItem key={_id}>
-                                <StyledImage src={image} alt='no image' />
-                                <Box sx={{ paddingLeft: 5 }}>
-                                    <h3>Product: {name}</h3>
-                                    <h3>Quantity: {quantity}</h3>
-                                    <h3>Total: {quantity * price}$</h3>
-                                </Box>
-                            </StyledCartItem>
+                            <>
+                                <StyledCartItem key={_id}>
+                                    <StyledImage src={image} alt='no image' />
+                                    <Box sx={{ paddingLeft: 5 }}>
+                                        <h3>{t('product')}: {name}</h3>
+                                        <h3>{t('quantity')}: {quantity}</h3>
+                                        <h3>{t('total')}: {quantity * price}$</h3>
+                                    </Box>
+                                </StyledCartItem>
+                                <StyledButtonCointainer>
+                                    <Button onClick={() => {
+                                        dispatch(clearCart());
+                                        setIsCartOpen(false);
+                                    }}>{t('clear_cart')}</Button>
+                                    {userData && <Button onClick={() => {
+                                        dispatch(saveCart({ userId: userData?.user._id, cartItems }))
+                                    }}>Save</Button>}
+                                </StyledButtonCointainer>
+                            </>
                         );
                     })
                 ) : (
                     <p>No items in the cart.</p>
                 )}
             </LoadingWrapper>
-            <StyledButtonCointainer>
-                <Button onClick={() => {
-                    dispatch(clearCart());
-                    setIsCartOpen(false);
-                }}>Clear Cart</Button>
-                {userData && <Button onClick={() => {
-                    dispatch(saveCart({ userId: userData?.user._id, cartItems }))
-                }}>Save</Button>}
-            </StyledButtonCointainer>
+
         </Drawer>
     );
 };
