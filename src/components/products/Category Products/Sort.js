6263f4a104060262e93c4052b29a98ba8bb0d2f2
SabaMaghradze <sabamaghradze1001@gmail.com>
2024-01-05 16:36:41 +0400

    sort multilanguage support

--- src/components/products/Category Products/Sort.js
+++ src/components/products/Category Products/Sort.js
@@ -1,13 +1,17 @@
 import { MenuItem, Select } from '@mui/material';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const Sort = ({ value, changeSort }) => {
+
+    const { t } = useTranslation();
+
     return (
         <Select defaultValue='' value={value ?? 'price,desc'} onChange={(e) => { changeSort("sort", e.target.value) }} sx={{ marginLeft: '10px' }}>
-            <MenuItem value='price,desc'>Price Descending</MenuItem>
-            <MenuItem value='price,asc'>Price Ascending</MenuItem>
-            <MenuItem value='name,desc'>Name Descending</MenuItem>
-            <MenuItem value='name,asc'>Name Ascending</MenuItem>
+            <MenuItem value='price,desc'>{t('price_descending')}</MenuItem>
+            <MenuItem value='price,asc'>{t('price_ascending')}</MenuItem>
+            <MenuItem value='name,desc'>{t('sort_by_reverse_alphabet')}</MenuItem>
+            <MenuItem value='name,asc'>{t('sort_by_alphabet')}</MenuItem>
         </Select>
     )
 };
